d5dca1aef7cb3130c2c21f9f7ad7fe77ca3d26f2
Clara <rustiktie@gmail.com>
2017-10-10 20:15:51 +0200

    Player code

--- Dev_class5_handout/Motor2D/j1Player.cpp
+++ Dev_class5_handout/Motor2D/j1Player.cpp
@@ -1,11 +1,19 @@
 #include "j1Player.h"
 #include "SDL/include/SDL.h"
 #include "p2Log.h"
-
+#include "j1App.h"
+#include "j1Textures.h"
+#include "j1Input.h"
+#include "j1Render.h"
 
 
 j1Player::j1Player()
 {
+	idle.PushBack({0,0,30,46});
+	idle.loop = false;
+
+	pos.x = 10;
+	pos.y = 50;
 }
 
 
@@ -16,21 +24,44 @@ j1Player::~j1Player()
 bool j1Player::Start() 
 {
 	LOG("Loading Player");
+
+	graphics = App->tex->Load("assets/Pumpkin sprites.png");
+
+	return true;
 }
 
-bool j1Player::PreUpdate() 
-{
-}
 
 bool j1Player::Update() 
 {
+	speed = 1;
+
+	// MOVEMENT -------------------------------------------
+	//FOWARDS
+	if (App->input->GetKey(SDL_SCANCODE_D) == KEY_REPEAT) {
+		pos.x += speed;
+	}
+
+	//BACKWARDS
+	if (App->input->GetKey(SDL_SCANCODE_A) == KEY_REPEAT) {
+		pos.x -= speed;
+	}
+
+	current = &idle;
+
+	//DRAW PLAYER -----------------------------------------
+	App->render->Blit(graphics, pos.x, pos.y, &(current->GetCurrentFrame()), 0.75f);
+
+	return true;
+
 }
 
-bool j1Player::PostUpdate() 
-{
-}
 
 bool j1Player::CleanUp() 
 {
+	LOG("Unloading Player");
+
+	App->tex->UnLoad(graphics);
+
+	return true;
 }
 
--- Dev_class5_handout/Motor2D/j1Player.h
+++ Dev_class5_handout/Motor2D/j1Player.h
@@ -3,6 +3,8 @@
 #define __j1PLAYER_H__
 
 #include "j1Module.h"
+#include "Animation.h"
+#include "p2Point.h"
 struct SDL_Texture;
 
 class j1Player
@@ -12,14 +14,17 @@ public:
 	~j1Player();
 
 	bool Start();
-	bool PreUpdate();
 	bool Update();
-	bool PostUpdate();
 	bool CleanUp();
 
 private:
 	SDL_Texture* graphics = nullptr;
 	SDL_Texture* textures = nullptr;
+	Animation* current = nullptr;
+	Animation idle;
+
+	iPoint pos;
+	int speed;
 };
 
 #endif
