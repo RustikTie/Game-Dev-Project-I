781f9dc720f9ecbb4bf26ea76dd3e2e66d0de6b8
Clara <rustiktie@gmail.com>
2017-10-10 19:08:23 +0200

    started working on player functions

--- Dev_class5_handout/Motor2D/j1Player.cpp
+++ Dev_class5_handout/Motor2D/j1Player.cpp
@@ -1,4 +1,6 @@
 #include "j1Player.h"
+#include "SDL/include/SDL.h"
+#include "p2Log.h"
 
 
 
@@ -10,3 +12,25 @@ j1Player::j1Player()
 j1Player::~j1Player()
 {
 }
+
+bool j1Player::Start() 
+{
+	LOG("Loading Player");
+}
+
+bool j1Player::PreUpdate() 
+{
+}
+
+bool j1Player::Update() 
+{
+}
+
+bool j1Player::PostUpdate() 
+{
+}
+
+bool j1Player::CleanUp() 
+{
+}
+
--- Dev_class5_handout/Motor2D/j1Player.h
+++ Dev_class5_handout/Motor2D/j1Player.h
@@ -1,8 +1,25 @@
 #pragma once
+#ifndef __j1PLAYER_H__
+#define __j1PLAYER_H__
+
+#include "j1Module.h"
+struct SDL_Texture;
+
 class j1Player
 {
 public:
 	j1Player();
 	~j1Player();
+
+	bool Start();
+	bool PreUpdate();
+	bool Update();
+	bool PostUpdate();
+	bool CleanUp();
+
+private:
+	SDL_Texture* graphics = nullptr;
+	SDL_Texture* textures = nullptr;
 };
 
+#endif
